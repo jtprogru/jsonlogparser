3f3e6012a20cac83a0259e46bdc8947a6620ec9d
Ivan Anfilatov <ivan.anfilatov@karuna.group>
2020-04-25 22:46:23 +0300

    Rewrite to channels and goroutines

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 )
 
 const iso8601DateFormat = "2006-01-02T15:04:05-07:00"
@@ -28,33 +29,56 @@ type LogString struct {
 
 func main() {
 	// Check length of args
-	if len(os.Args) < 1 {
+	if len(os.Args) < 2 { //first arg is binary name
 		fmt.Println("ERROR: lost path to logfile \n" +
 			"./jsonlogparser ./path/to/access.log")
 	}
 	fName := os.Args[1]
 	// Try to open file
+
+	threadCount := runtime.NumCPU()
+
+	strCh := make(chan string, threadCount)
+	parsedCh := make(chan *LogString, threadCount)
+
+	go readFile(fName, strCh)
+	go parseJson(strCh, parsedCh)
+
+	someLogic(parsedCh)
+}
+
+func readFile(fName string, ch chan string) {
+	defer close(ch)
 	file, err := os.Open(fName)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("error: %v", err)
+		return
 	}
 	defer file.Close()
-	// prep - array of string
-	var prep []string
-	// Some magic from StackOverflow
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		prep = append(prep, scanner.Text())
+		ch <- scanner.Text()
 	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	if err = scanner.Err(); err != nil {
+		log.Printf("error: %v", err)
 	}
+}
 
-	fmt.Println(len(prep))
-	for k,_ := range prep {
-		str := []byte(string(prep[k]))
-		res := LogString{}
-		json.Unmarshal([]byte(str), &res)
-		fmt.Println(res)
+func parseJson(in <-chan string, out chan *LogString) {
+	defer close(out)
+	for str := range in {
+		res := new(LogString)
+		err := json.Unmarshal([]byte(str), res)
+		if err != nil {
+			log.Printf("Error on parse '%s': %v", str, err)
+		} else {
+			out <- res
+		}
+	}
+}
+
+func someLogic(in <-chan *LogString) {
+	for ls := range in {
+		log.Printf("%v", ls)
 	}
 }
