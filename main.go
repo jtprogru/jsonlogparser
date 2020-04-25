8a6c4f2870dd38a8a36f8ec3d980914e282a27bd
JTProg <mail@jtprog.ru>
2020-04-25 16:42:46 +0300

    Add some fkng magic;

--- main.go
+++ main.go
@@ -1,39 +1,58 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
-	"net"
-	"path"
 )
 
 const iso8601DateFormat = "2006-01-02T15:04:05-07:00"
 
 type LogString struct {
-	timestamp string `json:"@timestamp"`
-	remote_addr string `json:"remote_addr"`
-	cookie_bar string `json:"cookie_bar"`
-	set_cookie string `json:"set_cookie"`
-	body_bytes_sent string `json:"body_bytes_sent"`
-	status string `json:"status"`
-	request string `json:"request"`
-	url string `json:"url"`
-	request_method string `json:"request_method"`
-	upstream string `json:"upstream"`
-	response_time string `json:"response_time"`
-	http_referrer string `json:"http_referrer"`
-	http_user_agent string `json:"http_user_agent"`
+	Timestamp     string `json:"@timestamp"`
+	RemoteAddr    string `json:"remote_addr"`
+	CookieBar     string `json:"cookie_bar,omitempty"`
+	SetCookie     string `json:"set_cookie,omitempty"`
+	BodyByteSent  string `json:"body_bytes_sent"`
+	Status        string `json:"status"`
+	Request       string `json:"request"`
+	URL           string `json:"url"`
+	RequestMethod string `json:"request_method"`
+	Upstream      string `json:"upstream"`
+	ResponseTime  string `json:"response_time"`
+	HTTPReferer   string `json:"http_referrer,omitempty"`
+	HTTPUserAgent string `json:"http_user_agent"`
 }
 
 func main() {
-	f_name := os.Args[1]
-
-	_, f := path.Split(f_name)
-	fmt.Println(f)
-	file, err := os.Open(f_name)
+	if len(os.Args) < 1 {
+		fmt.Println("ERROR")
+	}
+	fName := os.Args[1]
+	file, err := os.Open(fName)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	defer file.Close()
+	prep := []string{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		prep = append(prep, scanner.Text())
+		//fmt.Println(scanner.Text())
+		//fmt.Println(len(prep))
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(len(prep))
+	for k,_ := range prep {
+		str := []byte(string(prep[k]))
+		res := LogString{}
+		json.Unmarshal([]byte(str), &res)
+		fmt.Println(res)
+	}
 }
